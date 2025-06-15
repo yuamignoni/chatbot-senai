7d07f3f2fb94f3bd54061f3766051478b0e26707
dudaborges <duda.pborges92@gmail.com>
2025-06-15 20:02:57 -0300

    :rocket: feat: t: feat: login implementation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 // src/server.ts
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import authRoutes from './routes/authRoutes'; 
 
 dotenv.config(); 
 
 const app = express();
+app.use(cors());
 app.use(express.json()); 
 
 const PORT = process.env.PORT || 3000;
